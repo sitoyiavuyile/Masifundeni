96469c7c99fadb1b48ea7504f135f3bab88a1633
samthepessimist <231116144@mycput.ac.za>
2026-05-24 00:37:44 +0200

    registered middleware aliases

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,8 +10,18 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        
+        $middleware->alias([
+            'role'       => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+
+            
+            'admin'      => \App\Http\Middleware\EnsureAdmin::class,
+            'instructor' => \App\Http\Middleware\EnsureInstructor::class,
+            'student'    => \App\Http\Middleware\EnsureStudent::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
